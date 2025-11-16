91175b316d776f3d525071afc33f2a36fa569a83
b01817143 <b01817143@studentmail.uws.ac.uk>
2025-11-16 16:56:40 +0000

    changes mmade
    a

--- babylonProj/motion01/src/keyActionManager.ts
+++ babylonProj/motion01/src/keyActionManager.ts
@@ -2,33 +2,42 @@ import { ExecuteCodeAction } from "@babylonjs/core/Actions";
 import { ActionManager } from "@babylonjs/core/Actions/actionManager";
 import { Scene } from "@babylonjs/core/scene";
 
-export let keyDownMap: Boolean[] = [];
-let keyDown:number = 0;
-export function keyDownHeld(){ keyDown = 2}; 
-export function getKeyDown():number {return keyDown};
+export let keyDownMap: Record<string, boolean> = {};
+let keyDown: number = 0;
+
+export function keyDownHeld() {
+  keyDown = 2;
+};
+
+export function getKeyDown(): number {
+  return keyDown;
+};
 
 export function keyActionManager(scene: Scene) {
+
   scene.actionManager.registerAction(
     new ExecuteCodeAction(
-      {
-        trigger: ActionManager.OnKeyDownTrigger,
-      },
-      function (evt) {
-        if (keyDown == 0){keyDown ++}
-        keyDownMap[evt.sourceEvent.key] = true;
+      { trigger: ActionManager.OnKeyDownTrigger },
+      (evt) => {
+        if (keyDown === 0) keyDown = 1;
+        const key = evt.sourceEvent.key;
+        keyDownMap[key] = true;
       }
     )
   );
+
   scene.actionManager.registerAction(
     new ExecuteCodeAction(
-      {
-        trigger: ActionManager.OnKeyUpTrigger,
-      },
-      function (evt) {
+      { trigger: ActionManager.OnKeyUpTrigger },
+      (evt) => {
         keyDown = 0;
-        keyDownMap[evt.sourceEvent.key] = false;
+        const key = evt.sourceEvent.key;
+        keyDownMap[key] = false;
       }
     )
   );
+
   return scene.actionManager;
 }
+
+
